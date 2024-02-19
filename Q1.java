51d65039dc8d1db1e27f472cd3155ff62d4abec2
Lugafe <1349537@sga.pucminas.br>
2024-02-19 14:13:39 -0300

    Palindromo

--- Q1.java
+++ Q1.java
@@ -1,7 +1,37 @@
-/**
- * Q1
- */
-public class Q1 {
+import java.util.*;;
 
-    
+
+public class Q1 {
+    public static void main(String[] args) {
+        String palavra = MyIO.readLine();
+
+        do {
+            if (!palavra.equalsIgnoreCase("FIM")) {
+                if (palindrom(palavra)) {
+                    System.out.println("SIM");
+                }
+                else{
+                    System.out.println("NAO");
+                }   
+            }
+            palavra = MyIO.readLine();            
+        } while (!palavra.equalsIgnoreCase("FIM"));
+
+    }
+
+    private static boolean palindrom(String palavra) {
+        
+        int fim = palavra.length()-1;
+        int i = 0;
+        boolean ok = true;
+        while (i < fim) {
+            if (palavra.charAt(i) != palavra.charAt(fim)) {
+                ok = false;
+            }
+            fim--;
+            i++;
+        }
+        
+        return ok;        
+    }    
 }
