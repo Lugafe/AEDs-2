c3b0ed7e4c2c218ceeb2a78747f0f4f27eb2b346
Lugafe <lugafe2187@gmail.com>
2024-02-21 14:21:24 -0300

    Palindromo - C

--- Q2.c
+++ Q2.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
-/*
-bool palindrom(char *p){
-    int i = 0;    
-    int fim = sizeof(palavra);
-    bool ok = true;
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-    while (i < fim)
+bool palindrom(char *p)
+{
+    int i = 0;
+    int fim = strlen(p) - 1;
+    bool ok = true;
+    
+    while (i < fim/2)
     {
-        if (palavra[i] != palavra[fim])
+        if (p[i] != p[fim])
         {
             ok = false;
         }
         fim--;
-        i++;        
+        i++;
     }
     return ok;
 }
-*/
-int main(){
-    char *p;
-    p = (char *)malloc(5*sizeof(char));
-    scanf("%s", &p); 
-    printf("%s", p);
 
-    
+int main()
+{
+    char *p = (char *)malloc(10 * sizeof(char));
+    scanf("%s", p);
+    do
+    {
+        if (strcmp(p, "FIM") != 0)
+        {
+            if (palindrom(p))
+            {
+                printf("SIM\n");
+            }
+            else
+            {
+                printf("NAO\n");
+            }
+        }
+        scanf("%s", p);
+
+    } while (strcmp(p, "FIM") != 0);
+
     free(p);
-
 }
