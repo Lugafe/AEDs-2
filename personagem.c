ca937ca631f51a1f7ab01e3646db303560a0ce3f
Lugafe <1349537@sga.pucminas.br>
2024-04-03 11:40:29 -0300

    TP1 e TP2

--- personagem.c
+++ personagem.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+typedef struct personagem
+{
+    char id[500];
+    char name[500];
+    char alternate_names[500];
+    char house[500];															
+    char ancestry[500];
+    char species[500];
+    char patronus[500];
+    bool hogwartsStaff;
+    bool hogwartsStudent;
+    char actorName[500];
+    bool alive;
+    char alternate_actors[500];
+    char dateOfBirth[500];
+    int yearOfBirth;
+    char eyeColour[500];    
+    char gender[500];
+    char hairColour[500];
+    bool wizard;
+};
+
+
 int main(){
     
     char S[500];
