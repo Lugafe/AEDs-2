cdb3ca3587239b189aeef3b0970ff9c89a8b49b3
Lugafe <lugafe2187@gmail.com>
2024-05-06 21:09:15 -0300

    v2

--- TP2/Q13TP2.java
+++ TP2/Q13TP2.java
@@ -1,19 +1,22 @@
+package TP2;
 
 import java.io.IOException;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.*;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
 
-public class Teste {
+public class Q13TP2 {
    public static void main(String[] args) throws Exception {
       Scanner sc = new Scanner(System.in);
       Lista personagens = new Lista();
       Personagem p = new Personagem();
-      //String ids[] = new String[500];
+      String ids[] = new String[500];
       Lista personagens2 = new Lista();
       String s = "";
-      
+      int i = 0;
+      // recebe os valores dos ids
       personagens = arquivo();
       s = sc.nextLine();
       do {
@@ -22,11 +25,9 @@ public class Teste {
             personagens2.inserirFim(p);
             p = new Personagem();
             s = sc.nextLine();
-            
          }
       } while (!s.equalsIgnoreCase("FIM"));
-      personagens2.selecao();
-      personagens2.selecao();
+      personagens2.sort();
       personagens2.mostrar();
    }
 
@@ -35,7 +36,7 @@ public class Teste {
       File file = new File("/tmp/characters.csv");
       Lista personagens = new Lista();
       Personagem p = new Personagem();
-      //String line;
+      String line;
       try {
          Scanner sc = new Scanner(file);
          while (sc.hasNextLine()) {
@@ -128,15 +129,12 @@ class Personagem {
 
    }
 
-   // [9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8 ## Harry Potter ## {The Boy Who Lived,
-   // The Chosen One, Undesirable No. 1, Potty} ## Gryffindor ## half-blood ##
-   // human ## stag ## false ## false ## Daniel Radcliffe ## false ## 31-07-1980 ##
-   // 1980 ## green ## male ## black ## false]
    public void imprimir() {
       String datas = arruma(this.dateOfBirth);
       // String atores = nomeacao(this.alternate_actors);
       System.out
-            .println("[" + this.id + " ## " + this.name + " ## " + this.alternate_names + " ## " + this.house + " ## "
+            .println("[" + this.id + " ## " + this.name + " ## " + this.alternate_names + " ## " + this.house
+                  + " ## "
                   + this.ancestry + " ## " + this.species + " ## " + this.patronus + " ## " + this.hogwartsStaff
                   + " ## " + this.hogwartsStudent + " ## " + this.actorName
                   + " ## " + this.alive + " ## " + datas + " ## " + this.yearOfBirth + " ## "
@@ -144,17 +142,11 @@ class Personagem {
                   + this.gender + " ## " + this.hairColour + " ## " + this.wizard + "]");
    }
 
-   private String arruma(LocalDate d) {
+   public String arruma(LocalDate d) {
       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dataString = d.format(formatter);
-        return dataString.replace("/","-");
-        /* 
-        String[] nova = dataString.split("/");
-        String aux = nova[0];
-        nova[0] = nova[2];
-        nova[2] = aux;
-        return String.join("-", nova);
-        */
+      String dataString = d.format(formatter);
+      return dataString.replace("/", "-");
+
    }
 
    public String getId() {
@@ -314,25 +306,70 @@ class Lista {
       this(500);
    }
 
-   public void selecao() {
-      Personagem p = new Personagem();
-      for (int i = 0; i < 10; i++) {
-         if (array[i] != null) {
-            for (int j = 0; j < array.length; j++) {
-               if (array[j] != null) {
-                  if (array[i].getName().compareTo(array[j].getName()) > 0) {
-                     p = array[j];
-                     array[j] = array[i];
-                     array[i] = p;
-                  }
-               }
-            }
-         }
+   public void sort() {
+      mergesort(0, n-1);
+   }
+
+   private void mergesort(int esq, int dir) {
+      if (esq < dir){
+         int meio = (esq + dir) / 2;         
+         mergesort(esq, meio);
+         mergesort(meio + 1, dir);
+         intercalar(esq, meio, dir);
       }
-      
    }
 
    
+   public void intercalar(int esq, int meio, int dir){
+      int n1, n2, i, j, k;
+
+      //Definir tamanho dos dois subarrays
+      n1 = meio-esq+1;
+      n2 = dir - meio;
+
+      Personagem[] a1 = new Personagem[n1+1];
+      Personagem[] a2 = new Personagem[n2+1];
+
+      //Inicializar primeiro subarray
+      for(i = 0; i < n1; i++){
+         a1[i] = array[esq+i];
+      }
+
+      //Inicializar segundo subarray
+      for(j = 0; j < n2; j++){
+         a2[j] = array[meio+j+1];
+      }
+
+      //Sentinela no final dos dois arrays
+      a1[i] = a2[j] = null;
+
+      //Intercalacao propriamente dita
+      for (k = esq, i = j = 0; k <= dir; k++) {
+    if (i < n1 && (j >= n2 || compara(a1[i], a2[j]) < 0)) {
+        array[k] = a1[i++];
+    } else {
+         array[k] = a2[j++];
+    }
+}
+   }
+
+   
+
+   private void log(int qRepeticoes, int nMovimentacoes) {
+      try (FileWriter myWriter = new FileWriter("793599_mergesort.txt")) {
+         myWriter.write("788060\t" + System.currentTimeMillis() + "\t" + qRepeticoes + "\t" + nMovimentacoes);
+         myWriter.close();
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
+
+   private int compara(Personagem j, Personagem tmp) {
+      int valor = j.getActorName().compareTo(tmp.getActorName());
+      return valor != 0 ? valor : j.getName().compareTo(tmp.getName());
+   }
+
    public Lista(int tamanho) {
       array = new Personagem[tamanho];
       n = 0;
@@ -340,7 +377,7 @@ class Lista {
 
    public void inserirFim(Personagem x) throws Exception {
 
-      // validar insercao
+      // validar heapsort
       if (n >= array.length) {
          throw new Exception("Erro ao inserir!");
       }
@@ -350,18 +387,13 @@ class Lista {
    }
 
    public void mostrar() {
-      for (int i = 0; i < 10; i++) {
-         array[i].imprimir();
+      for (int i = 0; i < n; i++) {
+         if (array[i] != null) {
+            array[i].imprimir();
+         }
       }
    }
 
-   /**
-    * Procura um elemento e retorna se ele existe.
-    * 
-    * @param x int elemento a ser pesquisado.
-    * @return <code>true</code> se o array existir,
-    *         <code>false</code> em caso contrario.
-    */
    public boolean pesquisar(Personagem x) {
       boolean retorno = false;
       for (int i = 0; i < n && retorno == false; i++) {
@@ -401,3 +433,4 @@ class Lista {
       return retorno;
    }
 }
+
